73f0db84acaf089bd558e83875a2ffc078b2c244
David Huertas <davidhuertasortiz@gmail.com>
2023-08-02 03:40:33 +0200

    add: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import nodejs from '@astrojs/node';
 import react from '@astrojs/react';
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   output: 'hybrid',
   integrations: [tailwind({
     applyBaseStyles: false
