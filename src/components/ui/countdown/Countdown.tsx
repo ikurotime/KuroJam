21f6d01a48d813c0072f0dcbd9cf78d8db7c1eca
Jarrison Cano <jarrison.cano@hotmail.com>
2023-08-10 14:55:26 -0500

    change: home

--- src/components/ui/countdown/Countdown.tsx
+++ src/components/ui/countdown/Countdown.tsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import './styles.css'
 
-const dateLocale = new Date('2023-08-10T20:00:00+02:00').toLocaleDateString(
+const dateLocale = new Date('2023-08-23T11:59:00+02:00').toLocaleDateString(
   'default',
   { hourCycle: 'h24', hour: '2-digit', minute: '2-digit' }
 )
 const COUNTDOWN_TARGET = new Date(dateLocale).getTime()
-const ZERO_VALUE = 0
 const getTimeLeft = () => {
   const totalTimeLeft = COUNTDOWN_TARGET - new Date().getTime()
-  const días = ZERO_VALUE
+  const días = Math.floor(totalTimeLeft / (1000 * 60 * 60 * 24))
   const horas = Math.floor((totalTimeLeft / (1000 * 60 * 60)) % 24)
   const minutos = Math.floor((totalTimeLeft / (1000 * 60)) % 60)
   const segundos = Math.floor((totalTimeLeft / 1000) % 60)
@@ -30,7 +29,29 @@ const Countdown = () => {
   }, [])
 
   return (
-    <div className="countdown">
+    <>
+      <div className="flex gap-2 text-lg">
+        <div className="countdown-box">
+          <span>{timeLeft.días}</span>
+          <span>d</span>
+        </div>
+        <span>:</span>
+        <div className="countdown-box">
+          <span>{timeLeft.horas}</span>
+          <span>h</span>
+        </div>
+        <span>:</span>
+        <div className="countdown-box">
+          <span>{timeLeft.minutos}</span>
+          <span>m</span>
+        </div>
+        <span>:</span>
+        <div className="countdown-box">
+          <span>{timeLeft.segundos}</span>
+          <span>s</span>
+        </div>
+      </div>
+      {/* <div className="countdown">
       <div className="content grid grid-cols-2 grid-rows-2 place-items-center sm:flex">
         {Object.entries(timeLeft).map((el) => {
           const label = el[0]
@@ -45,7 +66,8 @@ const Countdown = () => {
           )
         })}
       </div>
-    </div>
+      </div> */}
+    </>
   )
 }
 
